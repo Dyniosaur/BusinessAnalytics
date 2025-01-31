ae875c5d0210296905e9099c6ac425b789cceed7
Dyniosaur <pauldynio@gmail.com>
2025-01-31 22:00:01 +0100

    Adding the Blur

--- businessanalytics/src/App.js
+++ businessanalytics/src/App.js
@@ -9,8 +9,22 @@ function App() {
 
 
   return(
-    <div style={{ maxWidth: '600px', maxHeight: '400px', justifySelf:'center', alignSelf: 'center' }}>
+    <div>
+
+
+      <div style={{width: '1300px', height: '1300px', position:'absolute', left: '-700px', top: '-700px', backgroundColor: '#281EE3', borderRadius: "100%", opacity:'15%' }} className='blurOne'>
+      </div>
+
+
+      <div style={{width: '1300px', height: '1300px', position:'absolute', left: '-600px', top: '-600px', backgroundColor: '#7251EA', borderRadius: "100%", opacity:'9%' }} className='blurTwo'>
+      </div>
+
+
+      <div style={{ maxWidth: '600px', maxHeight: '400px', justifySelf:'center', alignSelf: 'center' }}>
       <LineGraph data={data} />
+      </div>
+
+      
     </div>
   )
 }
