7d8e891751282627d4eea479b39013fca885c9e4
Dyniosaur <pauldynio@gmail.com>
2025-01-31 18:55:54 +0100

    First Graph Functionnal

--- businessanalytics/src/App.js
+++ businessanalytics/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-import {LineGraph} from "./components/Line"
+import LineGraph from "./components/Line"
 
 function App() {
+  const data = {
+    labels: ["XS", "S", "M", "L", "XL"],
+    values: [20000, 75000, 90000, 50000, 15000],
+  };
+
+
   return(
-    <div>
-      <LineGraph/>
+    <div style={{ maxWidth: '600px', maxHeight: '400px', justifySelf:'center', alignSelf: 'center' }}>
+      <LineGraph data={data} />
     </div>
   )
 }
--- businessanalytics/src/components/Line.js
+++ businessanalytics/src/components/Line.js
@@ -1,7 +1,6 @@
-import {Line} from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Legend, Title, Tooltip } from "chart.js";
-import { FetchedData } from "../FetchedData";
-import { useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Legend, Title, Tooltip, Colors } from "chart.js";
+
 
 ChartJS.register(
     CategoryScale,
@@ -15,16 +14,54 @@ ChartJS.register(
 
 
 
-export const LineGraph = () =>{
+const LineGraph = ({ data }) => {
 
-    useEffect( () => {
-        FetchedData(), []}
-    )
+    const chartData = {
+      labels: data.labels,
+      datasets: [
+        {
+          label: 'Sales Q1',
+          data: data.values,
+          borderColor: 'rgb(42, 199, 95)',
+          borderWidth: 1,
+          fill: false,
+          tension: 0.4,
+        },
+      ],
+    };
 
-    const options={}
+    const options = {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true,
+          grid : {
+            color: 'rgb(255, 255, 255)',
+          }
+        },
+        x: {
+            grid : {
+            color: 'rgb(255, 255, 255)',
+            }
+        }
+      },
+      plugins: {
+        legend: {
+          display: true,
+          position: 'top',
+        },
+        tooltip: {
+          enabled: true,
+        },
+      },
+    };
 
-    const data={FetchedData}
+    return (
+      <div>
+        <h2>Line Chart</h2>
+        <Line data={chartData} options={options} />
+      </div>
+    );
+};
 
-
-    return <Line options={options} data={data}/>
-}
+export default LineGraph;
