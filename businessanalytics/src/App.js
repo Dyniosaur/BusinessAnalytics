b408028447e1c27a762342aeee8d14bacab425ae
Dyniosaur <pauldynio@gmail.com>
2025-02-07 21:36:58 +0100

    Betterment from 0

--- businessanalytics/src/App.js
+++ businessanalytics/src/App.js
@@ -3,6 +3,7 @@ import BarGraph from './components/Bar';
 import PieGraph from './components/Pie';
 import LineGraph from "./components/Line"
 import Sidebar from "./components/Sidebar"
+import NavTop from './components/NavTop';
 
 function App() {
   const data = {
@@ -12,31 +13,22 @@ function App() {
 
 
   return(
-    <div>
-      <div className='déco'>
-        <div style={{width: '1300px', height: '1300px', position:'absolute', left: '-700px', top: '-700px', backgroundColor: '#007ea7', borderRadius: "100%", opacity:'4%',}} className='blurOne'>
-        </div>
-        <div style={{width: '1300px', height: '1300px', position:'absolute', left: '-600px', top: '-600px', backgroundColor: '#7251EA', borderRadius: "100%", opacity:'1%', }} className='blurTwo'>
-        </div>
-        <div style={{width: '1300px', height: '1300px', position:'absolute', right: '-700px', bottom: '-700px', backgroundColor: '#007ea7', borderRadius: "100%", opacity:'4%', }} className='blurOne'>
-        </div>
-        <div style={{width: '1300px', height: '1300px', position:'absolute', right: '-600px', bottom: '-600px', backgroundColor: '#7251EA', borderRadius: "100%", opacity:'1%',}} className='blurTwo'>
+    <div style={{height:'100%', width:'100%'}}>
+      <NavTop/>
+      <div className="big-grid">
+        <Sidebar/>
+
+        <div className="heroDashboard">
+          <div style={{width:'100%', height:'auto', backgroundColor:'rgb(46, 106, 255, 0.1)', borderRadius:'8px', padding:'12px'}}>
+          <LineGraph data={data}/>
+          </div>
+          <div style={{width:'400px', height:'auto', backgroundColor:'rgb(46, 106, 255, 0.1)', borderRadius:'8px', padding:'12px'}}>
+          <PieGraph data={data} />
+          </div>
+          <div style={{width:'100%', height:'auto', backgroundColor:'rgb(46, 106, 255, 0.1)', borderRadius:'8px', padding:'12px'}}>
+          <BarGraph data={data} />
         </div>
       </div>
-
-
-      <Sidebar/>
-
-      <div className="heroDashboard">
-        <div style={{ Width: '450px', Height: 'auto', fontFamily:'Roboto', color:'white', backgroundColor:'rgba(0, 0, 0, 0.16)', borderRadius:'16px', alignSelf:'center', justifySelf:'flex-end', }}>
-        <LineGraph data={data} />
-        </div>
-        <div style={{ Width: '250px', Height: 'auto', fontFamily:'Roboto', color:'white', backgroundColor:'rgba(0, 0, 0, 0.16)', borderRadius:'16px', alignSelf:'center', justifySelf:'flex-start',  }}>
-        <PieGraph data={data} />
-        </div>
-      </div>
-      <div style={{ minWidth: '40%', maxHeight: '100px', justifySelf:'center', alignSelf: 'center', fontFamily:'Roboto', color:'white', backgroundColor:'rgba(0, 0, 0, 0.16)', borderRadius:'16px', margin:'15px' }}>
-        <BarGraph data={data} />
       </div>
     </div>
   )
---
+++ businessanalytics/src/components/NavTop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavTop() {
+  return (
+    <div style={{marginLeft:'50%',marginRight:'50%', width:'100px', fontFamily:'Roboto', position:'absolute',borderBottom:'2px solid rgba(167, 167, 167, 0.121)'}}>
+      <h1 style={{justifySelf:'center', color: 'rgba(0, 0, 0, 0.71)'}}>LOGO</h1>
+    </div>
+  )
+}
+
+export default NavTop
